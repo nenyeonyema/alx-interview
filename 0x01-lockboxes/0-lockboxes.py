7511ae4c7987a393243cbf8c255a856b887bb55e
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-05 08:59:44 -0700

    locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Locked boxes """
+
+
+
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be opened
+    """
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
