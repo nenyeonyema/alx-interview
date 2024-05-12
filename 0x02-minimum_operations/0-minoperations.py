7aad29d7ed438868e2ab017ad291859292c3f6d7
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-12 15:09:12 -0700

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,15 +9,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
-    dp = [float('inf')] * (n + 1)  # Initialize a dynamic programming array
-    
+
+    dp = [float('inf')] * (n + 1)
+
     # Base cases
-    dp[1] = 0  # To achieve one 'H' character, no operations are needed
-    
+    dp[1] = 0
+
     for i in range(2, n + 1):
         for j in range(1, i):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + i // j)
-    
     return dp[n]
