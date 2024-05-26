5aa276ae9431cc423b1caf00dea7753d59aa3320
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-26 07:34:59 -0700

    validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,8 @@
 
 def validUTF8(data):
     """ UTF8 Validation """
+
+
     def is_valid_byte(byte):
         """validate byte """
         return 0 <= byte <= 255
@@ -15,7 +17,7 @@ def validUTF8(data):
         byte = data[i]
         if not is_valid_byte(byte):
             return False
-        
+
         # Determine the number of bytes in this character
         num_bytes = 0
         if (byte >> 7) == 0:  # 1-byte character
@@ -28,14 +30,14 @@ def validUTF8(data):
             num_bytes = 4
         else:
             return False
-        
+
         if i + num_bytes > n:
             return False
-        
+
         for j in range(1, num_bytes):
             if (data[i + j] >> 6) != 0b10:
                 return False
-        
+
         i += num_bytes
-    
+
     return True
